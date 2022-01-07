c9dff9536af0403e4ad9aa8e79e03df44a3878e2
Christoph Scholz <christoph.serrig@gmail.com>
2022-01-07 18:29:14 +0100

    updated cards

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ npm run lint
 
 
 if [ $? -eq 0 ]; then
-	./node_modules/browserify/bin/cmd.js src/client_js/*.js -o build/main.js
+	./node_modules/browserify/bin/cmd.js src/*/*.js -o build/main.js
 	./node_modules/less/bin/lessc --clean-css src/styles/*.less build/main.min.css
 	./node_modules/terser/bin/terser build/main.js --compress --mangle --comments=false -o build/main.min.js
 	rm build/main.js 
